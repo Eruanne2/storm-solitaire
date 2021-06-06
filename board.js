21ea8df98dfdf5fd03cf75f38167950e5effdb1a
Charis <bluegrassgal222@gmail.com>
2021-06-06 12:51:54 -0600

    write drag and drop event handlers, abstract board class

---
+++ board.js
@@ -0,0 +1,52 @@
+const Card = require('./card.js');
+
+const fullDeck = [
+  'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH',
+  'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD',
+  'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS',
+  'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC',
+]
+
+class Board {
+  constructor() {
+    this.hand = fullDeck.map(str => new Card(str))
+    this.shuffle(this.hand);
+    this.waste = []
+    this.tableau = [[], [], [], [], [], [], []]
+    this.foundations = {'H': [], 'D': [], 'S': [], 'C': []}
+  }
+
+  shuffle(arr) {
+    var currentIdx = arr.length;
+    var temp, randomIdx;
+    while (currentIdx !== 0) {
+      randomIdx = Math.floor(Math.random() * currentIdx);
+      currentIdx -= 1;
+      temp = arr[currentIdx];
+      arr[currentIdx] = arr[randomIdx];
+      arr[randomIdx] = temp;
+    }
+    return arr
+  }
+
+  flipHand() {
+    this.waste.push(this.hand.pop())
+  }
+
+  restartHand() {
+    while (this.waste.length > 0) {
+      this.hand.unshift(this.waste.shift())
+    }
+  }
+
+  moveCardToTableau(card, stack) {
+
+  }
+
+  moveCardToFoundations(card) {
+    
+  }
+  
+}
+
+module.exports = Board;
--- game.js
+++ game.js
@@ -1,45 +1,30 @@
-const Card = require('./card.js');
-
-const fullDeck = [
-  'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH',
-  'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD',
-  'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS',
-  'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC',
-]
+const Board = require('./board.js');
 
 class Game {
   constructor() {
-    this.hand = fullDeck.map(str => new Card(str))
-    this.shuffle(this.hand);
-    this.waste = []
-    this.tableau = [[], [], [], [], [], [], []]
-    this.foundations = [[], [], [], []]
-  }
+    this.board = new Board;
 
-  shuffle(arr) {
-    var currentIdx = arr.length;
-    var temp, randomIdx;
-    while (currentIdx !== 0) {
-      randomIdx = Math.floor(Math.random() * currentIdx);
-      currentIdx -= 1;
-      temp = arr[currentIdx];
-      arr[currentIdx] = arr[randomIdx];
-      arr[randomIdx] = temp;
-    }
-    return arr
+    // set event handlers
   }
-
-  flipHand() {
-    this.waste.push(this.hand.pop())
-  }
-
-  restartHand() {
-    while (this.waste.length > 0) {
-      this.hand.unshift(this.waste.shift())
-    }
-  }
-
   
+  dragFromWaste(e) {
+    e.dataTransfer.setData('card', this.board.waste[this.board.waste.length-1])
+  }
   
+  dragFromTableau(e) {
+    let stack = e.currentTarget; // which stack is it dragged from
+    e.dataTransfer.setData('card', this.board.tableau[stack][this.board.tableau[stack].length-1])
+  }
+  
+  dropOnFoundations(e) {
+    e.preventDefault();
+    let card = e.dataTransfer.getData('card');
+  }
+  
+  dropOnTableau(e) {
+    e.preventDefault();
+    let card = e.dataTransfer.getData('card');
+    let stack = e.currentTarget; // which stack is it being dropped on
+  }
+
 }
-
